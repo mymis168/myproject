b9cb9b66e6ede36a7ded6d515d8df42069c31b33
Michael <mymis168@gmail.com>
2026-09-02 10:31:39 +0800

    image update 2

--- streamlit_web1.py
+++ streamlit_web1.py
@@ -18,7 +18,7 @@ with c1:
 
 st.divider()
 st.header("肺癌臨床治死亡率比較")
-st.image("images/lung.png", caption="肺癌臨床治療存活率比較圖", use_column_width=True)
+st.image("images/l2.jpg", caption="肺癌臨床治療存活率比較圖", use_column_width=True)
 
 # Layout (B) Sidebar
 with st.sidebar:
